ab6e1063406e8357ab5347d744ee0ce8ee162a0b
steezo <fstewart9@gmail.com>
2025-01-26 11:49:41 -0500

    added for loop and comparison to only show owned zombies

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -21,7 +21,22 @@ contract ZombieHelper is ZombieFeeding {
 
   function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // Start here
+    
+    // Declare a uint called counter and set it equal to 0.
+    uint counter = 0;
+    
+    // Declare a for loop that starts from uint i=0 and goes up through i < zombies.length.
+    for (uint i=0; i < zombies.length; i++) {
+        
+        // Make an if statement that checks if zombieToOwner[i] is equal to _owner.
+        // This will compare the two addresses to see if there is a match.
+        if (zombieToOwner[i] = _owner) {
+            result[counter] = i; 
+            counter++;
+        }
+    }
+    
+    // The function should now return all the zombies owned by _owner without spending any gas.
     return result;
   }
 
